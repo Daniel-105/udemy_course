b1463199b1778440e42d3776ce11d658c6282d32
Daniel Infante <danielpinto105@gmail.com>
2021-06-12 14:45:29 +0100

    first program in JS

--- javaScript/if_statments/app.js
+++ javaScript/if_statments/app.js
@@ -1,3 +1,33 @@
-if (1 + 1 === 2) {
-    console.log("Math still works")
+// let random = Math.random();
+// if (random < 0.8) {
+//     console.log("You number is less than 0.8");
+//     console.log(random);
+// }
+
+// let dayOfWeek = "Saturday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("Shit! Today is monday")
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("Great! Today  I'll rest!")
+// }
+
+const age = prompt("Insere a tua idade");
+if (age < 5) {
+    console.log("Dude, go home!")
+} else if (age <= 10) {
+    console.log("You are too young!")
+} else if (age <= 17) {
+    console.log("You are almost there! But wait a little bit longer")
+} else if (age >= 18) {
+    console.log("Welcome a board")
 }
+
+// const num = 14785236993;
+
+// if (num % 2 === 0) {
+
+// console.log("Your number is pair by two")
+// } else if (num % 2 ==! 0) {
+//     console.log("You're fucked")
+// }

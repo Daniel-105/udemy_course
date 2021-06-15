4435d7060a7d2b7502bb1b28ec2c108599296205
Daniel Infante <danielpinto105@gmail.com>
2021-06-15 15:31:47 +0100

    interactive program

--- javaScript/if_statments/app.js
+++ javaScript/if_statments/app.js
@@ -4,24 +4,34 @@
 //     console.log(random);
 // }
 
-// let dayOfWeek = "Saturday";
+const dayOfWeek = "monday";
 
-// if (dayOfWeek === "Monday") {
-//     console.log("Shit! Today is monday")
-// } else if (dayOfWeek === "Saturday") {
-//     console.log("Great! Today  I'll rest!")
+if (dayOfWeek === prompt("monday").toLowerCase()) {
+    console.log("Shit! Today is Monday") 
+} else if (prompt("saturday")) {
+    console.log("Great! Today  I'll rest!")
+} else {
+    console.log("MEH");
+}
+// } else if (dayOfWeek === "Tuesday") {
+//     console.log("MEH")
+// } else if (dayOfWeek === "Wednesday") {
+//     console.log("MEH")
+// } else if (dayOfWeek === "Thrusday") {
+//     console.log("MEH")
 // }
 
-const age = prompt("Insere a tua idade");
-if (age < 5) {
-    console.log("Dude, go home!")
-} else if (age <= 10) {
-    console.log("You are too young!")
-} else if (age <= 17) {
-    console.log("You are almost there! But wait a little bit longer")
-} else if (age >= 18) {
-    console.log("Welcome a board")
-}
+
+// const age = prompt("Insere a tua idade");
+// if (age < 5) {
+//     console.log("Dude, go home!")
+// } else if (age <= 10) {
+//     console.log("You are too young!")
+// } else if (age <= 17) {
+//     console.log("You are almost there! But wait a little bit longer")
+// } else if (age >= 18) {
+//     console.log("Welcome a board")
+// }
 
 // const num = 14785236993;
 
@@ -31,3 +41,12 @@ if (age < 5) {
 // } else if (num % 2 ==! 0) {
 //     console.log("You're fucked")
 // }
+
+// const age = 9;
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free")
+// } else if (age < 10) {     
+//     console.log("You are a child! You pay 10$")
+// } else if (age < 65) {
+//     console.log("You are a child! You pay 20$")
+// } 

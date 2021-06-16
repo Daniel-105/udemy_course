db82cc6133ec86c3e3c142be0688803a5883c99b
Daniel Infante <danielpinto105@gmail.com>
2021-06-16 12:35:56 +0100

    password program

--- javaScript/if_statments/app.js
+++ javaScript/if_statments/app.js
@@ -4,15 +4,15 @@
 //     console.log(random);
 // }
 
-const dayOfWeek = "monday";
+// const dayOfWeek = prompt("Enter you day").toLowerCase();
 
-if (dayOfWeek === prompt("monday").toLowerCase()) {
-    console.log("Shit! Today is Monday") 
-} else if (prompt("saturday")) {
-    console.log("Great! Today  I'll rest!")
-} else {
-    console.log("MEH");
-}
+// if (dayOfWeek === "monday")  {
+//     console.log("Shit! Today is Monday") 
+// } else if (prompt("saturday")) {
+//     console.log("Great! Today  I'll rest!")
+// } else {
+//     console.log("MEH");
+// }
 // } else if (dayOfWeek === "Tuesday") {
 //     console.log("MEH")
 // } else if (dayOfWeek === "Wednesday") {
@@ -42,11 +42,36 @@ if (dayOfWeek === prompt("monday").toLowerCase()) {
 //     console.log("You're fucked")
 // }
 
-// const age = 9;
+// const age = 90;
 // if (age < 5) {
 //     console.log("You are a baby. You get in for free")
 // } else if (age < 10) {     
 //     console.log("You are a child! You pay 10$")
 // } else if (age < 65) {
 //     console.log("You are a child! You pay 20$")
-// } 
+// } else {
+//     console.log("You are a senior! You pay 10$")
+// }
+
+// const num = Math.random();
+
+// if (num < 0.5) {
+//     console.log("Sorry!! Your number is less than 0.5");
+// } else {
+//     console.log("Great!! Your number is greater or equal than 0.5");
+// }
+// console.log(num);
+
+const password = prompt("Enter your new password");
+
+if (password.length >= 6) {
+    if (password.indexOf(" ") === -1){
+        console.log("Valid Password!");
+    } else {
+        console.log("No spaces please!");
+    }
+} else {
+    console.log("Too short");
+}
+
+

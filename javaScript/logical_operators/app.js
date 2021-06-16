c1bfa6386ce8cd8f3a6001c7c9b17c4d15d25683
Daniel Infante <danielpinto105@gmail.com>
2021-06-16 15:54:38 +0100

    logical operators

--- javaScript/logical_operators/app.js
+++ javaScript/logical_operators/app.js
@@ -1,1 +1,19 @@
-console.log("This is working");
+// const password = prompt("Enter your password");
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("Valid Password");
+// } else {
+//     alert("Invalid password")
+// }
+
+const age = prompt('Enter your age, please');
+
+if (age > 0 && age <= 5) {
+    console.log("You're a baby you enter for free")
+} else if (age > 5 && age <= 10 || age > 65 && age < 120) {
+    console.log('You pay $10');
+} else if (age > 10 && age <= 65) {
+    console.log('You are an adult, you pay $20');
+} else {
+    alert("Invalid age");
+}

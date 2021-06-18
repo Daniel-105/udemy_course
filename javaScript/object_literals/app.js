f69611a67c24f4dc3e719486685afb76721f6d72
Daniel Infante <danielpinto105@gmail.com>
2021-06-18 18:11:53 +0100

    first object program

---
+++ javaScript/object_literals/app.js
@@ -0,0 +1,15 @@
+let fisrtName = prompt("Enter you first name, please");
+let surName = prompt("Enter you sur name, please");
+let age = prompt("Enter you age, please");
+let city = prompt("Enter you city name, please");
+let favColors = [prompt("Enter your favorit colors, please")];
+
+let data = {
+    firstName: fisrtName,
+    surName: surName,
+    age: age,
+    city: city,
+    favColors: favColors
+}
+
+console.log(data);

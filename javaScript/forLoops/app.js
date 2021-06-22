c95d4af9d6d7f1280d0f39074eafe0025aa50297
Daniel Infante <danielpinto105@gmail.com>
2021-06-22 15:47:02 +0100

    learning for loops

--- javaScript/forLoops/app.js
+++ javaScript/forLoops/app.js
@@ -1,9 +1,63 @@
-for (let i = 100; i >= 0; i -= 10) {
-    console.log(i)
-}
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i)
+// }
 
 
 // This is a infinit loop! DO NOT USE THIS!!!
 // for (let i = 1; i >= 0; i++) {
 //     console.log(i)
 // }
+
+// let weddingList = ['maria', 'tiago', 'Inês', 'Daniel',
+//  'Nuno Jesus',
+//   'Paizinho',
+//    'Mãezinha']
+
+// weddingList.splice(0, 2, 'Maria', 'Tiago');
+
+// for (let i = 0; i < weddingList.length; i++) {
+//     console.log(i, weddingList[i]);
+
+// }
+
+// for (let i = 6; i >= 0; i--) {
+//     console.log(i, weddingList[i])
+// }
+
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+
+// for (let i = 0; i <= 3; i++) {
+//     console.log(people[i].toUpperCase())
+// }
+
+// for (let i = 10; i >= 10 && i <= 20; i++) {
+//     console.log(`i is:${i}`)
+//     for (j = 1; j <= 5; j++) {
+//         console.log(`       j is: ${j}`)
+//     }
+// }
+
+
+
+// const seatingChart = [
+//     ['Kristen', 'Eric', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erica']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     console.log(`ROW #${i + 1}`)
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j])
+//     }
+// }
+
+for (let i = 0; i <= 10; i++) {
+    // for(let j = i + 1; j < 10; j++) {
+    //     console.log(i, j)
+    // }
+    console.log(i)
+} 

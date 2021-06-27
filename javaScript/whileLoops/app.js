bc78c0ef913f1553907a535b870448ac0c0c82bb
Daniel Infante <danielpinto105@gmail.com>
2021-06-27 19:35:38 +0100

    guessing game by me own

--- javaScript/whileLoops/app.js
+++ javaScript/whileLoops/app.js
@@ -14,32 +14,67 @@
 // }
 // console.log('CONGRATS!!')
 
-let maxNum = parseInt(prompt('Enter your maximum number:'));
+// let maxNum = parseInt(prompt('Enter your maximum number:'));
 
+
+// while (!maxNum) {
+
+//     maxNum = parseInt(prompt('Enter a valid number:'));
+// }
+
+// const targetNum = Math.floor((Math.random() * maxNum)) + 1;
+
+// let guess = prompt('Enter your guess');
+// let attemps = 1;
+
+// while (parseInt(guess) !== targetNum) {
+//     if (guess === 'q') break;
+//     attemps++
+//     if (guess > targetNum) {
+//         guess = prompt('Too high. Guess again');
+//     } else {
+//         guess = prompt('Too low. Guess again');  
+//     }
+// }
+// if (guess === 'q') {
+//     console.log('OK, You quit')
+// } else {
+//     console.log(`You got it! It took you ${attemps} guesses. The number is ${targetNum}`)
+// }
+
+
+
+
+
+
+
+let maxNum = parseInt(prompt('Please enter you max number:'));
 
 while (!maxNum) {
-
-    maxNum = parseInt(prompt('Enter a valid number:'));
+    maxNum = parseInt(prompt('Please enter you max number:'))
 }
 
-const targetNum = Math.floor((Math.random() * maxNum)) + 1;
+let targetNum = Math.floor(Math.random() * maxNum) + 1;
 
-let guess = prompt('Enter your guess');
+let guess = prompt('Enter your guess:');
 let attemps = 1;
 
 while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
     attemps++
-    if (guess > targetNum) {
-        guess = prompt('Too high. Guess again');
+    if (guess === 'q') break
+    else if (guess > targetNum) {
+        guess = prompt('Too high! Try again:');
     } else {
-        guess = prompt('Too low. Guess again');  
+        guess = prompt('Too low! Try again:');
     }
 }
-if (guess === 'q') {
-    console.log('OK, You quit')
-} else {
-    console.log(`You got it! It took you ${attemps} guesses. The number is ${targetNum}`)
 
+if (guess === 'q') {
+    console.log('You have quit')
+} else {
+    console.log(`Congratulations! The number was ${targetNum}!`);
+console.log(`It took you ${attemps} guesses!`);
 }
+
+
 
